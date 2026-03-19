c75f1fe415534e9b690782be50c6c1fad3952f4b
Elliot Jones <elliot.jones103@gmail.com>
2026-03-19 23:21:52 +0000

    random samples from normal distribution introduced

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@ int main() {
     float T = 1.0;
     int num_simulations = 1000;
 
-    std::cout << "Initial stock price: " << S_0 << std::endl;
-    std::cout << "Strike price: " << K << std::endl;
-    std::cout << "Risk-free rate: " << r << std::endl;
-    std::cout << "Volatility: " << sigma << std::endl;
-    std::cout << "Time to maturity: " << T << std::endl;
-    std::cout << "Number of simulations: " << num_simulations << std::endl;
+    // random number generator from normal distirbution 
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<float> dist(0, 1);
 
+    float Z = dist(gen);
+
+    std::cout << "Random normal Z: " << Z << std::endl;
+    
+    
     return 0;
 
 }
