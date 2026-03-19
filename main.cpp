6e0e13a360284d78f28b6023aa15b237cc1befe9
Elliot Jones <elliot.jones103@gmail.com>
2026-03-19 23:46:19 +0000

    final additions before end of coding session

--- main.cpp
+++ main.cpp
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <cmath>
 #include <random>
+#include <algorithm>
 
 int main() {
-    float S_0 = 100.0;
-    float K = 100.0;
-    float r = 0.05;
-    float sigma = 0.2;
-    float T = 1.0;
+    double S_0 = 100.0;
+    double K = 100.0;
+    double r = 0.05;
+    double sigma = 0.2;
+    double T = 1.0;
     int num_simulations = 1000;
 
     // random number generator from normal distirbution 
     
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::normal_distribution<float> dist(0, 1);
+    std::normal_distribution<double> dist(0, 1);
 
-    float Z = dist(gen);
+    double Z = dist(gen);
+
+    double S_T = S_0 * std::exp((r - 0.5 * pow(sigma, 2)) * T + sigma * std::sqrt(T) * Z);
+
+    double payoff = std::max(S_T - S_0, 0.0);
 
     std::cout << "Random normal Z: " << Z << std::endl;
-    
-    
+    std::cout << "Price at T: " << S_T << std::endl;
+    std::cout << "payoff: " << payoff << std::endl;
+
+
     return 0;
 
 }
